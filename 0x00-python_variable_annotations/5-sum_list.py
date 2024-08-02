a4f7e0a7ded1d82408b6b042f0bdbe9e9b645452
meron6 <khozameron5@gmail.com>
2024-08-02 13:30:39 -0700

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,14 +3,8 @@
 '''
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     '''Computes the sum of a list of floating-point numbers.
     '''
     return float(sum(input_list))
-
-# Example usage:
-if __name__ == "__main__":
-    example_list = [1.1, 2.2, 3.3]
-    result = sum_list(example_list)
-    print(f"The sum of {example_list} is {result}")
-
