3c0ead0537a8aa62cb1667d41820098195bd53ef
meron6 <khozameron5@gmail.com>
2024-08-02 13:28:46 -0700

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-"""Task 5: Complex types - list of floats."""
+'''Task 5: Complex types - list of floats
+'''
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
-    """Compute the sum of a list of floating-point numbers."""
-    return sum(input_list)
+    '''Computes the sum of a list of floating-point numbers.
+    '''
+    return float(sum(input_list))
+
+# Example usage:
+if __name__ == "__main__":
+    example_list = [1.1, 2.2, 3.3]
+    result = sum_list(example_list)
+    print(f"The sum of {example_list} is {result}")
+
