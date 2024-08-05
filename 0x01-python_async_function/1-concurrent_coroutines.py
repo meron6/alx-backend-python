bb41d0b25ea02c339cf45ec8cf829eb2433fa402
meron6 <khozameron5@gmail.com>
2024-08-05 13:31:24 -0700

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-"""execute multiple coroutines at the same time with async"""
+"""Contains a method that spawns wait_random n times with a
+specified delay between each call."""
 import asyncio
-from typing import list
+from typing import List
 
-wait_random = __import__('0-basic_asymcio_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-"""
-2 int arguments (in this order): and max_delay
-async routine called wait_n
 
-"""
-tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-return [await task for task in asyncio.as_completed(task)]
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with a specified delay
+    between each call.
+    Args:
+        n: number of times to spawn wait_random
+        max_delay: maximum delay between each call
+    Returns:
+        list of delays
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)
